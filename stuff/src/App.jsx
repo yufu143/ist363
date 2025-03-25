5644a558ba9a6d2e2503afaf58925497fac58d29
yufu143 <yfu143@syr.edu>
2025-03-25 11:48:23 -0400

    Update App.jsx

--- stuff/src/App.jsx
+++ stuff/src/App.jsx
@@ -1,4 +1,10 @@
+//global variable - every component can see it
+const myGlobalVariable = "I'm global baby!";
+
+
 function App() {
+//local variable - only works inside the component
+const myLocalVariable = "I'm local :-(";
   return (
     <div>
       <h1>Hello World</h1>
