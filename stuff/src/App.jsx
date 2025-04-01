4d3b4a3a1ca76adb587ee2cf5abc6c9116c399c6
yufu143 <yfu143@syr.edu>
2025-04-01 11:18:04 -0400

    Update App.jsx

--- stuff/src/App.jsx
+++ stuff/src/App.jsx
@@ -1,17 +1,9 @@
-//global variable - every component can see it
-const myGlobalVariable = "I'm global baby!";
-
 function App() {
-//local variable - only works inside the component
-const myLocalVariable = "I'm local :-(";
   return (
     <div>
       <h1>Hello World</h1>
-      <p>{myGlobalVariable}</p>
-      <p>{myLocalVariable}</p>
-      <p>separate<br /> lines with br tag that needs a trailing slash</p>
     </div>
   );
 }
 
-export default App;
+export default App
