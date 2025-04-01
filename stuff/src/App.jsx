bfb64e46ae067387ccf211ad43ee3bcede211dc6
yufu143 <yfu143@syr.edu>
2025-04-01 11:22:34 -0400

    Update App.jsx

--- stuff/src/App.jsx
+++ stuff/src/App.jsx
@@ -1,7 +1,21 @@
+// arrow function long form
+const Hello2 = () => {
+  return (
+    <div>
+      <h1>Hello 2</h1>
+    </div>
+  );
+}
+
+// arrow function concise
+const Hello3 = () => { <div><h1>Hello3</h1></div> }
+
 function App() {
   return (
     <div>
       <h1>Hello World</h1>
+      <Hello2 />
+      <Hello3 />
     </div>
   );
 }
