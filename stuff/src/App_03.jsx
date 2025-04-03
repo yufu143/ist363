941f401622d32ff330c1dbb8878918780bb40b24
yufu143 <yfu143@syr.edu>
2025-04-03 11:47:10 -0400

    Update App_03.jsx

--- stuff/src/App_03.jsx
+++ stuff/src/App_03.jsx
@@ -1,8 +1,8 @@
 const students = [
-    {suid: 123456, name: 'Sue Flay', year: 'senior', major: 'Applied Data Analytics'}
-    {}
-    {}
-]
+    {suid: 123456, name: 'Sue Flay', year: 'senior', major: 'Applied Data Analytics'},
+    {suid: 234567, name: 'Ella Vader', year: 'junior', major: 'Applied Data Analytics'},
+    {suid: 345678, name: 'Chris P Bacon', year: 'junior', major: 'Innovation, Society and Technology'}
+];
 
 function App() {
     let filteredStudents = students;
