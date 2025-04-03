de5b95709989d01900d2c7633406657b94e339f5
yufu143 <yfu143@syr.edu>
2025-04-03 11:45:16 -0400

    Update App_03.jsx

--- stuff/src/App_03.jsx
+++ stuff/src/App_03.jsx
@@ -1,42 +1,32 @@
-// brings in react useState code
-import { useState } from "react";
+const students = [
+    {suid: 123456, name: 'Sue Flay', year: 'senior', major: 'Applied Data Analytics'}
+    {}
+    {}
+]
 
-// sets up useState, what variable, what function changes it, and what start value
-const Home = () => {
-  const [name, setName]  = useState('Yujia');
-
-let handleClick = () => {
-  setName(Yujia);
-}
-
-  return (
-    <div className="home">
-      <h1>Hello World</h1>
-      <p>{name}</p>
-      <button onClick={handleClick}>Update</button>
-    </div>
-  );
-}
-
-/*
-  return (
-    <div>
-      <h1>Hello World</h1>
-      {name}
-      <button onClick={handleClick}>Update</button>
-    </div>
-  );
-}
-*/
-
-/*
 function App() {
-  let name = "Yujia";
-  const handleClick = () => {
-    name = "Amanda"
-    // check to show this function runs
-    console.log(name);
-  }
-*/
+    let filteredStudents = students;
 
-export default App
+    const handleChange = (event) => {
+        filteredStudents = students.filter(student => student.name === event.target.value);
+        console.log(filteredStudents);
+    }
+    return (
+      <div>
+        <label htmlFor="search">Search:</label>
+        <input id="search" type="text" onChange={handleChange} />
+      </div>
+      <ul>
+        filteredStudents.map(function(item) {
+        return
+        <li key={item.suid}>
+            Name: {item.name}<br />
+            Year: {item.year}<br />
+            Major: {item.major}<br />
+        </li>;
+        })}
+      </ul>
+    );
+  }
+  
+  export default App;
