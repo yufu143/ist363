13881e8183140a7f8400be697d3bf905f725acdc
yufu143 <yfu143@syr.edu>
2025-04-03 12:02:36 -0400

    Update App_03.jsx

--- stuff/src/App_03.jsx
+++ stuff/src/App_03.jsx
@@ -5,28 +5,27 @@ const students = [
 ];
 
 function App() {
-    let filteredStudents = students;
-
     const handleChange = (event) => {
-        filteredStudents = students.filter(student => student.name === event.target.value);
-        console.log(filteredStudents);
-    }
-    return (
-      <div>
-        <label htmlFor="search">Search:</label>
-        <input id="search" type="text" onChange={handleChange} />
-      </div>
-      <ul>
-        filteredStudents.map(function(item) {
+        };
+
+return (
+<div>
+    <label htmlFor="search">Search:</label>
+    <input id="search" type="text" onChange={handleChange} />
+    <h1>Students</h1>
+    <ul>
+        {
+        filteredStudents.map(function(item){
         return
         <li key={item.suid}>
             Name: {item.name}<br />
             Year: {item.year}<br />
             Major: {item.major}<br />
         </li>;
-        })}
-      </ul>
-    );
-  }
+    })}
+    </ul>
+</div>
+        );
+    };
   
   export default App;
