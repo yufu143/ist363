1f4efa0b2102a46aa6f80ed1ed2b845d6bfef29f
yufu143 <yfu143@syr.edu>
2025-04-08 11:17:50 -0400

    Create App_?.jsx

---
+++ stuff/src/App_?.jsx
@@ -0,0 +1,42 @@
+// brings in react useState code
+import { useState } from "react";
+
+// sets up useState, what variable, what function changes it, and what start value
+const Home = () => {
+  const [name, setName]  = useState('Yujia');
+
+let handleClick = () => {
+  setName(Yujia);
+}
+
+  return (
+    <div className="home">
+      <h1>Hello World</h1>
+      <p>{name}</p>
+      <button onClick={handleClick}>Update</button>
+    </div>
+  );
+}
+
+/*
+  return (
+    <div>
+      <h1>Hello World</h1>
+      {name}
+      <button onClick={handleClick}>Update</button>
+    </div>
+  );
+}
+*/
+
+/*
+function App() {
+  let name = "Yujia";
+  const handleClick = () => {
+    name = "Amanda"
+    // check to show this function runs
+    console.log(name);
+  }
+*/
+
+export default App
